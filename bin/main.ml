84974e18db35fb6d60cc2a70e824254f24232e96
Daniel Stamer <stamer@google.com>
2024-09-04 15:21:59 +0000

    Add cli interface

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,94 @@
 open Semnodes
 
-let monitors = 
+let monitors =
   [
-    { number = 0; name = "bsh"; icon = ""; nodes = 
+    { number = 1; name = "bsh"; icon = ""; nodes =
       [
         { executable = "/usr/bin/xterm"; arguments = [] };
         { executable = "/usr/bin/xterm"; arguments = [] };
         { executable = "/usr/bin/xterm"; arguments = [] }
       ]
     };
-    { number = 1; name = "vim"; icon = ""; nodes = 
+    { number = 2; name = "vim"; icon = ""; nodes =
       [
         { executable = "/usr/bin/xterm"; arguments = [ "-e"; "nvim" ] }
       ]
     };
-    { number = 2; name = "doc"; icon = "󱔗"; nodes = 
+    { number = 3; name = "doc"; icon = "󱔗"; nodes =
       [
-        { executable = "/usr/bin/google-chrome"; arguments = [ "google.com" ] }
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "1"; "-u";
+            "cloud.google.com/go/docs/reference/cloud.google.com/go/latest" ] 
+        };
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "1"; "-u"; "pkg.go.dev" ] }
+      ]
+    };
+    { number = 4; name = "gcp"; icon = ""; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "2"; "-g"; "console.cloud" ] 
+        }
+      ]
+    };
+    { number = 5; name = "add"; icon = ""; nodes = []
+    };
+    { number = 6; name = "web"; icon = ""; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "1"] 
+        }
+      ]
+    };
+    { number = 7; name = "com"; icon = "󰺻"; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "2"; "-g"; "mail" ] 
+        }
+      ]
+    };
+    { number = 8; name = "cal"; icon = "󰃰"; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "2"; "-g"; "calendar" ] 
+        }
+      ]
+    };
+    { number = 9; name = "not"; icon = ""; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "2"; "stamer:notes" ] 
+        }
+      ]
+    };
+    { number = 10; name = "gvc"; icon = ""; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "2"; "companion" ] 
+        }
+      ]
+    };
+    { number = 11; name = "pers"; icon = ""; nodes =
+      [
+        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+          [ "-p"; "3"] 
+        }
       ]
     }
   ]
 
-let () = print_endline (monitors_to_string monitors)
+let usage = "semnodes [-cl] [monitor-name]"
+let toggle_launch = ref false
+let monitor_name = ref ""
+let set_monitor_name name = monitor_name := name
+let option_list = [("-l", Arg.Set toggle_launch, "Show launch commands for [monitor-name]")]
+
+let evaluate monitor launch =
+  match monitor with
+  | "" -> print_endline (monitors_to_string monitors)
+  | name -> print_endline (evaluate_command monitors name launch)
+
+let () =
+  Arg.parse option_list set_monitor_name usage;
+  evaluate !monitor_name !toggle_launch;
+
--- lib/semnodes.ml
+++ lib/semnodes.ml
@@ -8,16 +8,25 @@ let rec arguments_to_string args =
   match args with
   | [] -> ""
   | [arg] -> arg
-  | arg :: args -> arg ^ " " ^ arguments_to_string args;;
+  | arg :: args -> arg ^ " " ^ arguments_to_string args
 
-let node_to_string {executable;arguments} = executable ^ " " ^
-  arguments_to_string arguments;;
+let node_to_string {executable;arguments} =
+  executable ^ " " ^ arguments_to_string arguments
 
 let rec nodes_to_string nodes =
   match nodes with
   | [] -> ""
   | [node] -> (node_to_string node)
   | node :: nodes -> (node_to_string node) ^ "\n" ^ nodes_to_string nodes
+
+let node_to_command {executable;arguments} =
+  executable ^ " " ^ arguments_to_string arguments ^ " &"
+
+let rec nodes_to_command nodes =
+  match nodes with
+  | [] -> ""
+  | [node] -> (node_to_command node)
+  | node :: nodes -> (node_to_command node) ^ "\n" ^ nodes_to_command nodes
 
 type monitor =
   {
@@ -28,11 +37,32 @@ type monitor =
   }
 
 let monitor_to_string {number;name;icon;nodes} =
-  (Int.to_string number) ^ ": " ^ icon ^ " " ^ name ^ "\n" ^ nodes_to_string nodes;;
+  (Int.to_string number) ^ ": " ^ icon ^ " " ^ name ^ "\n" ^ nodes_to_string nodes
 
 let rec monitors_to_string monitors =
   match monitors with
   | [] -> ""
   | [monitor] -> monitor_to_string monitor
-  | monitor :: monitors -> (monitor_to_string monitor) ^ "\n" ^ monitors_to_string monitors;;
+  | monitor :: monitors -> (monitor_to_string monitor) ^ "\n" ^ monitors_to_string monitors
+
+let find_monitors name monitors = List.filter (fun x -> x.name = name) monitors
+
+let switch_to_command monitors name =
+  let monitors = find_monitors name monitors in
+  match monitors with
+  | [] -> "echo 'no op' # monitor not found"
+  | [x] -> "bspc desktop -f '^" ^ Int.to_string x.number ^ "'"
+  | _ -> "echo 'no op' # double config found"
+
+let launch_to_command monitors name =
+  let monitors = find_monitors name monitors in
+  match monitors with
+  | [] -> "echo 'no op' # monitor not found"
+  | [x] -> switch_to_command monitors name ^ "\n" ^ nodes_to_command x.nodes
+  | _ -> "echo 'no op' # double config found"
+
+let evaluate_command monitors name launch =
+  match launch with
+  | false -> switch_to_command monitors name
+  | true -> launch_to_command monitors name
 
