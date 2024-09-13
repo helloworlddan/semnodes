09ab544dda4dbcfac45245670d95ccf3ee050fde
Daniel Stamer <stamer@google.com>
2024-09-13 08:14:47 +0000

    Move to relative PATKs

--- bin/main.ml
+++ bin/main.ml
@@ -4,29 +4,29 @@ let monitors =
   [
     { number = 1; name = "bsh"; icon = ""; nodes =
       [
-        { executable = "/usr/bin/xterm"; arguments = [] };
-        { executable = "/usr/bin/xterm"; arguments = [] };
-        { executable = "/usr/bin/xterm"; arguments = [] }
+        { executable = "xterm"; arguments = [] };
+        { executable = "xterm"; arguments = [] };
+        { executable = "xterm"; arguments = [] }
       ]
     };
     { number = 2; name = "vim"; icon = ""; nodes =
       [
-        { executable = "/usr/bin/xterm"; arguments = [ "-e"; "nvim" ] }
+        { executable = "xterm"; arguments = [ "-e"; "nvim" ] }
       ]
     };
     { number = 3; name = "doc"; icon = "󱔗"; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "-u";
             "cloud.google.com/go/docs/reference/cloud.google.com/go/latest" ] 
         };
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "-u"; "pkg.go.dev" ] }
       ]
     };
     { number = 4; name = "gcp"; icon = ""; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "2"; "-g"; "console.cloud" ] 
         }
       ]
@@ -35,42 +35,42 @@ let monitors =
     };
     { number = 6; name = "web"; icon = ""; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"] 
         }
       ]
     };
     { number = 7; name = "com"; icon = "󰺻"; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "-g"; "mail" ] 
         }
       ]
     };
     { number = 8; name = "cal"; icon = "󰃰"; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "-g"; "calendar" ] 
         }
       ]
     };
     { number = 9; name = "not"; icon = ""; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "stamer:notes" ] 
         }
       ]
     };
     { number = 10; name = "gvc"; icon = ""; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "1"; "companion" ] 
         }
       ]
     };
     { number = 11; name = "pers"; icon = ""; nodes =
       [
-        { executable = "/usr/local/google/home/stamer/.go/bin/goto"; arguments =
+        { executable = "goto"; arguments =
           [ "-p"; "3"] 
         }
       ]
